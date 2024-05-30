6701cd5dcbb1532c3f4a4644518f3626f9108705
mohamad-askri <90568851+mohamad-askri@users.noreply.github.com>
2024-05-30 23:54:48 +0330

    update and install

--- project code/coinwatch-project/src/components/TradingViewWidget.jsx
+++ project code/coinwatch-project/src/components/TradingViewWidget.jsx
@@ -1,44 +1,5 @@
-/*
-import React, {useState} from 'react';
-import { TechnicalAnalysis, Ticker } from 'react-ts-tradingview-widgets';
-
-const TradingViewWidget = ({ darkMode }) => {
-
-    const colorTheme = darkMode ? "dark" : "dark";
 
 
-
-
-
-    const tickerStyles = {
-
-        parent: {
-            display:'none'
-        },
-
-    };
-
-    return (
-        <>
-            <TechnicalAnalysis
-                colorTheme={colorTheme}
-                width="100%"
-                height="400"
-                isTransparent={true}
-                copyrightStyles={tickerStyles}
-                symbol="CRYPTOCAP:TOTAL"
-
-            />
-
-
-
-        </>
-    );
-};
-
-export default TradingViewWidget;
-*/
-import React, { useState, useEffect } from 'react';
 import { TechnicalAnalysis } from 'react-ts-tradingview-widgets';
 
  const TradingViewWidget = ({darkMode}) => {
