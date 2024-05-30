c873b2feccccd72dd5a36fd3bbde315fa3c6a8a7
mohamad-askri <90568851+mohamad-askri@users.noreply.github.com>
2024-05-30 23:31:48 +0330

    Create Footer.jsx

---
+++ project code/coinwatch-project/src/components/Footer.jsx
@@ -0,0 +1,40 @@
+
+
+import './destination/Styles/style.css'
+import './destination/Bootstrap/css/bootstrap.min.css'
+import './destination/Styles/font.css'
+import './destination/Styles/dark-style.css'
+import './destination/Fonts/css/all.css'
+
+import {Tickertapewidget} from "./Tickertapewidget.jsx"
+
+
+
+
+function Footer ({darkMode})  {
+
+        return (
+
+
+<>
+                <footer className="copy-right text-center p-2">
+
+
+                    <a href="http://localhost:3000/AboutUs.html">
+                        <p>Made with ❤️</p>
+
+                    </a>
+
+
+                </footer>
+
+
+
+           <Tickertapewidget  darkMode={darkMode}   />
+
+</>
+        );
+    }
+
+
+export default Footer;
