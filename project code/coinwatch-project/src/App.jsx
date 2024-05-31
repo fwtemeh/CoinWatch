0d4bd4ab7f240bf9419dbc7be6ba84cf64a780c5
mohamad-askri <90568851+mohamad-askri@users.noreply.github.com>
2024-05-31 11:35:45 +0330

    update for save darkmode

--- project code/coinwatch-project/src/App.jsx
+++ project code/coinwatch-project/src/App.jsx
@@ -1,3 +1,4 @@
+/*
 
 
 import {Header} from './components/Header'
@@ -65,3 +66,58 @@ function App() {
 }
 
 export default App
+*/
+
+
+import { useState, useEffect } from "react";
+import { Header } from './components/Header';
+import { Mainpage } from './components/Mainpage.jsx';
+import { Routes, Route } from "react-router-dom";
+import { Coinpage } from "./components/Coinpage.jsx";
+import { Setting } from "./components/Setting.jsx";
+import Footer from "./components/Footer.jsx";
+
+
+
+ 
+
+
+function App() {
+    const [darkMode, setDarkMode] = useState(() => {
+        const savedMode = localStorage.getItem('darkMode');
+        return savedMode === 'true' || false;
+    });
+
+    const toggleDarkMode = () => {
+        setDarkMode(prevMode => {
+            const newMode = !prevMode;
+            localStorage.setItem('darkMode', newMode);
+            return newMode;
+        });
+    };
+
+    useEffect(() => {
+        const body = document.querySelector('body');
+        if (darkMode) {
+            body.classList.add('body_dark');
+        } else {
+            body.classList.remove('body_dark');
+        }
+    }, [darkMode]);
+
+    return (
+        <>
+            <div className={`extension-wrapper ${darkMode ? 'body_dark' : ''}`}>
+                <Header />
+                <Routes>
+                    <Route index path="*" element={<Mainpage darkMode={darkMode} />} exact />
+                    <Route path="/setting" element={<Setting toggleDarkMode={toggleDarkMode} darkMode={darkMode} />} exact />
+                    <Route path="/coins/:id" element={<Coinpage darkMode={darkMode} />} />
+                </Routes>
+                <Footer darkMode={darkMode} />
+            </div>
+        </>
+    );
+}
+
+export default App;
--- project code/coinwatch-project/src/components/Setting.jsx
+++ project code/coinwatch-project/src/components/Setting.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+/*import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export function Setting({ toggleDarkMode, darkMode }) {
@@ -56,9 +56,9 @@ export function Setting({ toggleDarkMode, darkMode }) {
                 </div>
 
         );
-    }
-
+    }*/
 /*
+
 import {useNavigate} from "react-router-dom";
 
 
@@ -112,3 +112,49 @@ export function Setting({toggleDarkMode ,darkMode}) {
     }
 */
 
+import { useNavigate } from "react-router-dom";
+
+
+
+export function Setting({ toggleDarkMode, darkMode }) {
+    let navigate = useNavigate();
+
+    const handleToggleDarkMode = (e) => {
+        toggleDarkMode(e.target.checked);
+    };
+
+    return (
+        <div className="settings-page">
+            <section className="mb-5" style={{ paddingBottom: '300px' }} id="extension-settings">
+                <div className="container-fluid">
+                    <div className="section-header text-start text-uppercase mb-3">
+                        <h2>Extension Settings</h2>
+                        <button type="button" className="back-button" onClick={() => navigate('/')}><i className="fa-solid fa-chevron-left"></i> Back</button>
+                    </div>
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="extension-card">
+                                <div className="settings-item-card">
+                                    <div className="settings-item-key">
+                                        <div className="setting-item-icon">
+                                            <i className="fa-solid fa-moon"></i>
+                                        </div>
+                                        <h3>
+                                            Dark Mode
+                                        </h3>
+                                    </div>
+                                    <div className="settings-item-value">
+                                        <label className="switch">
+                                            <input type="checkbox" onChange={handleToggleDarkMode} checked={darkMode} />
+                                            <span className="slider round"></span>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
