1b96ffedc9bc882355362611771fa4e5eba61396
mohamad-askri <90568851+mohamad-askri@users.noreply.github.com>
2024-05-31 11:36:51 +0330

    update
    delete comment code

--- project code/coinwatch-project/src/App.jsx
+++ project code/coinwatch-project/src/App.jsx
@@ -1,72 +1,3 @@
-/*
-
-
-import {Header} from './components/Header'
-import {Mainpage} from './components/Mainpage.jsx'
-import {Routes,Route} from "react-router-dom";
-import {Coinpage} from "./components/Coinpage.jsx";
-import {Setting} from "./components/Setting.jsx";
-import Footer from "./components/Footer.jsx";
-
-import { useState} from "react";
-
-
-
-
-
-
-
-function App() {
-
-    const [darkMode, setDarkMode] = useState(false);
-
-
-    const toggleDarkMode = () => {
-        setDarkMode(prevMode => !prevMode);
-        const body = document.querySelector('body');
-        body.classList.toggle('body_dark');
-    }
-
-    return (
-        <>
-            <body className={darkMode ? 'body_dark' : ''}>
-
-            <div className="extension-wrapper">
-
-
-
-
-
-                    <Header/>
-
-                    <Routes>
-
-                        <Route index  path="*" element={<Mainpage      darkMode={darkMode}       />} exact />
-
-
-                        <Route path="/setting" element={<Setting toggleDarkMode={toggleDarkMode} darkMode={darkMode} />} exact />
-
-                        <Route  path="/coins/:id"  element={<Coinpage darkMode={darkMode} />} />
-
-                    </Routes>
-
-
-
-
-
-
-
-
-                <Footer darkMode={darkMode} />
-
-            </div>
-            </body>
-        </>
-    )
-}
-
-export default App
-*/
 
 
 import { useState, useEffect } from "react";
@@ -79,7 +10,7 @@ import Footer from "./components/Footer.jsx";
 
 
 
- 
+
 
 
 function App() {
